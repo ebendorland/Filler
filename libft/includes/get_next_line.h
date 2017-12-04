3eb09d557ec68d6436f04567738c410246a8c58d
Eben <eben.dorland@gmail.com>
2017-12-04 22:37:50 +0200

    Nothing

---
+++ libft/includes/get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arnovan- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/06/07 10:01:03 by arnovan-          #+#    #+#             */
+/*   Updated: 2016/06/28 09:16:58 by kchetty          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_GET_NEXT_LINE_H
+# define FT_GET_NEXT_LINE_H
+# define BUFF_SIZE 1024
+# include "libft.h"
+
+int					get_next_line(const int fd, char **line);
+#endif
